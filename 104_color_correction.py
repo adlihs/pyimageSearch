1cc4de996c6fe72aa2a212bf7139510eabc75b11
Edmund Neil <edmondneil@gmail.com>
2023-02-22 09:24:08 -0600

    104_color_correction

--- 104_color_correction.py
+++ 104_color_correction.py
@@ -7,9 +7,10 @@ import imutils
 import cv2
 import sys
 
+
 def find_color_card(image):
     # load the ArUco dictionary grab the ArUCo parameters and detect the markers in the input image
-    arucoDict = cv2.aruco_Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
+    arucoDict = cv2.aruco_Dictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
 
@@ -31,5 +32,66 @@ def find_color_card(image):
         i = np.squeeze(np.where(ids == 241))
         bottomRight = np.squeeze(corners[i])[2]
 
+        # extract the bottom-left marker
+        i = np.squeeze(np.where(ids == 1007))
+        bottomLeft = np.squeeze(corners[i])[3]
+
+    # we could not find color correction card, so gracefully return
+    except:
+        return None
+
+    # build our list of reference points and apply a perspective
+    # transform to obtain a top-down, birds-eye-view of the color
+    # matching card
+    cardCoords = np.array([topLeft, topRight,
+                           bottomRight, bottomLeft])
+    card = four_point_transform(image, cardCoords)
+
+    # retunr the color matching card to the calling function
+    return card
 
 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-r", "--reference", required=True,
+                help="path to the input reference image")
+ap.add_argument("-i", "--input", required=True,
+                help="path to the input image to apply color correction to")
+args = vars(ap.parse_args())
+
+# load the reference image and input images from disk
+print("[INFO] loading images...")
+ref = cv2.imread(args["reference"])
+image = cv2.imread(args["input"])
+
+# Resize the reference and input images
+ref = imutils.resize(ref, width=600)
+image = imutils.resize(image, width=600)
+
+# display the reference and input images to our screen
+cv2.imshow("Reference", ref)
+cv2.imshow("Input", image)
+
+# find the color matching card in each image
+print("[INFO] finding color matching cards...")
+refCard = find_color_card(ref)
+imageCard = find_color_card(image)
+
+# if the color matching card is not found in either the reference
+# image or the input image gracefully exit
+if refCard is None or imageCard is None:
+    print("[INFO] could not find color matching card in both images")
+    sys.exit(0)
+
+# show the color matching card in the reference image and input image respectively
+cv2.imshow("Reference color card", refCard)
+cv2.imshow("Input color card", imageCard)
+
+# apply histogram matching from the color matching card in the
+# reference image to the color matching card in the input image
+print("[INFO] matching images...")
+imageCard = exposure.match_histograms(imageCard, refCard, multichannel=True)
+
+# show our input color matching card after histogram matching
+cv2.imshow("Input color card after matching", imageCard)
+cv2.waitKey(0)
